1e683dd76307d5b621031afa14acbbe32a4cd223
sandeep <sandeepsingh23ok@gmail.com>
2024-03-16 20:51:12 -0300

    Edit to solve problem- terrible code though

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,40 @@
-const removeFromArray = function() {
+const removeFromArray = function(someArray) {
 
-};
+    if (arguments.length >1 ){
+        let i=1;
+        let index = [];
+        while (i<arguments.length){
+            for(let j=0; j<someArray.length; j++){
+                if(someArray[j]===arguments[i]){
+                    index.push(j);
+                }
+                else{
+                    continue;
+                }
 
+            }
+            i++;
+            }
+            let indexSorted = index.sort();
+            console.log(indexSorted);
+            let k =indexSorted.length-1;
+            while(k>=0){
+                someArray.splice(indexSorted[k],1);
+                k--;
+            
+            }
+            return someArray;
+            
+        }
+        
+        
+
+    else{
+            return "argument not in array";
+        }
+    }
+        
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 2))
 // Do not edit below this line
 module.exports = removeFromArray;
