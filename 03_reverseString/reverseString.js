ec0a1d06aedf44aca20b50f2be521ec23e640ab9
sandeep <sandeepsingh23ok@gmail.com>
2024-03-16 15:38:12 -0300

    Edit to pass tests using while loop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let strLength = string.length -1;
+    let newString = "";
+    while(strLength>=0){
+        newString += string.charAt(strLength);
+        strLength-- ;
+    }
+    return newString;
 };
-
+console.log(reverseString("hello"));
 // Do not edit below this line
 module.exports = reverseString;
