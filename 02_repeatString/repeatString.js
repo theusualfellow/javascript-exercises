8bc2f5901ed4f905bfac72c3288e56dd2be03734
sandeep <sandeepsingh23ok@gmail.com>
2024-03-15 22:41:39 -0300

    Use repeat instead of loop to pass tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+        if(num<0) return "ERROR"
+        else{string = string.repeat(num);}
+        
+    
+    return string;
 };
 
+console.log(repeatString('hey',4));
 // Do not edit below this line
 module.exports = repeatString;
