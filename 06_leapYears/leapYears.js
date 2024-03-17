0a1925f374c0e8146290e51b7248f77de3f25fba
sandeep <sandeepsingh23ok@gmail.com>
2024-03-17 02:09:08 -0300

    Edit to pass the tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if((year % 4 == 0 && year %100 >0) || (year % 400 ==0)){
+        return true
+    }
+    else{
+        return false
+    }
 };
 
+console.log(leapYears(1800))
 // Do not edit below this line
 module.exports = leapYears;
