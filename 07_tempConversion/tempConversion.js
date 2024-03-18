5bce2ab6fe0f4198e6bd7fbcba45b5e0f0a7c1c7
sandeep <sandeepsingh23ok@gmail.com>
2024-03-18 20:08:24 -0300

    Edit to pass tests using Math round method

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let celcius= (farenheit - 32)*5/9
+  let roundCelcius = Math.round(celcius *10)/10
+  return roundCelcius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let farenheit = (celcius * 9/5) + 32
+  let roundFarenheit = Math.round(farenheit*10)/10
+  return roundFarenheit;
 };
 
+
+console.log(convertToFahrenheit(22))
+console.log(convertToCelsius(100))
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
