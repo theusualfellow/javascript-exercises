f7747fa7a475c1bde9d316ca3d3a784bc37ff576
sandeep <sandeepsingh23ok@gmail.com>
2024-03-24 18:17:56 -0300

    Modify to solve exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const noSpace=string.toLowerCase().replace(/[,\s.!]/g, "")
+    const reverseWord = noSpace.split("").reduce((firstWord, lastWord)=> lastWord+firstWord);
+    console.log(noSpace)
+    console.log(reverseWord)
+    if(noSpace==reverseWord){
+        return true
+    }
+    else{
+        return false
+    } 
 
 };
 
+console.log(palindromes("sandeep"))
 // Do not edit below this line
 module.exports = palindromes;
