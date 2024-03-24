034c6d10a4939efdb030649d3344b4fc151938a1
sandeep <sandeepsingh23ok@gmail.com>
2024-03-24 16:09:05 -0300

    Modify to solve exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current)=>{
+    return total+current
+  },0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, current)=>product*current,1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  const array1= []
+  for(let i=number; i>0; i--){
+    array1.push(i)
+  }
+	return array1.reduce((total, current)=>total*current,1)
 };
+
+console.log(factorial(3))
 
 // Do not edit below this line
 module.exports = {
