a942bda95c23da5496eca3e5501744026e0b7e3c
sandeep <sandeepsingh23ok@gmail.com>
2024-03-24 18:49:30 -0300

    Edit to pass the tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    strictNum= Number(number)
+    if (strictNum<0 || isNaN(strictNum)){
+        return "OOPS"
+    }
+    if(strictNum==0){
+        return 0
+    }
+    let array = [1,1]
+    for(let i=0; i<strictNum; i++){
+        
+        array.push(array[i]+array[i+1])
+    }
+    return array[strictNum-1]
 };
-
+console.log(fibonacci("yo"))
 // Do not edit below this line
 module.exports = fibonacci;
