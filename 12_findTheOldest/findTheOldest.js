1c02a3998a954b650868648a5a8eea4b63fb7da3
sandeep <sandeepsingh23ok@gmail.com>
2024-03-25 21:44:45 -0300

    Solve using sort array method

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(people){
+    const today = new Date()
+    const year = today.getFullYear()
+    const newArray= people
+    .sort(function(person1, person2){
+        if(person1.yearOfDeath == undefined){
+            person1.yearOfDeath = year
+        }
+        else if(person2.yearOfDeath == undefined){
+            person2.yearOfDeath = year
+        }
+        else{
+            const age1 = person1.yearOfDeath-person1.yearOfBirth
+            const age2 = person2.yearOfDeath-person2.yearOfBirth
+            return age2-age1
+        }
+        const age1 = person1.yearOfDeath-person1.yearOfBirth
+        const age2 = person2.yearOfDeath-person2.yearOfBirth
+        return age2-age1
 
-};
-
+        
+    }
+    
+    )
+    return newArray[0]
+    }
+   
+  console.log(findTheOldest(people))
 // Do not edit below this line
 module.exports = findTheOldest;
