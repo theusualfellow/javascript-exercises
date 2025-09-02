f424f49b1813eb56f6d13ee8dd13e2ab67e53277
sandeep <sandeepsingh23ok@gmail.com>
2025-09-01 23:44:39 -0300

    Solve using string and array methods

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    return str.split('').reverse().join('');
 };
+
+
+
+
+console.log(reverseString('hello words'));
+
 
 // Do not edit below this line
 module.exports = reverseString;
