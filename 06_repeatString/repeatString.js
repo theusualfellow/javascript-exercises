5717f743fbf6a342e3f7806e8b16c8606764aa9d
sandeep <sandeepsingh23ok@gmail.com>
2025-09-01 23:34:41 -0300

    Solve using array methods

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(str, numOfTimes) {
+    let arr=[];
+    if(numOfTimes<0){
+        return("ERROR");        
+    }
+    else{
+        for(let i =0; i<numOfTimes; i++){
+                
+                arr.push(str);
+            }
+            return arr.join('');
+    }
+    
 };
+console.log(repeatString('hello',0));
 
 // Do not edit below this line
 module.exports = repeatString;
