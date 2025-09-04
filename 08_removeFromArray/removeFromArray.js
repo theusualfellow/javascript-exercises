73c8a3f94610f23d13cebb359788de49c4ab0664
sandeep <sandeepsingh23ok@gmail.com>
2025-09-03 21:45:22 -0300

    Solve exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...userInput) {
+    let elementsInCommon= userInput.filter(item=>arr.includes(item));
+    arr=arr.filter(item=>!elementsInCommon.includes(item));
+    return arr;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+
+console.log(removeFromArray([1,2,3,4,4], 4,3,9,'sandeep'));
