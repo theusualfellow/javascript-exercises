e0163513ff9ff19a29db6f15d29eedd6fea09579
sandeep <sandeepsingh23ok@gmail.com>
2025-09-03 22:04:29 -0300

    Solve using reduce and loops

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a,b) {  
+    let allNumbers = [];
+    if(Number.isInteger(a)&&Number.isInteger(b)&&a>0&&b>0){
 
+        if(a<b){
+            for(i=a; i<=b;i++){
+                console.log(i);
+                allNumbers.push(i);
+            }
+        return allNumbers.reduce((num1,num2)=>num1+num2);
+        }
+        else{
+            for(i=b; i<=a;i++){
+                console.log(i);
+                allNumbers.push(i);
+            }
+        return allNumbers.reduce((num1,num2)=>num1+num2);
+        }
+    }
+    else return('ERROR');
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+console.log(sumAll(1,5));
